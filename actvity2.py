8e3e67e38c712dacbf59b1d748eb2456d9b114aa
mahisingh4415 <mahisinghadityasingh1232@gmail.com>
2024-11-23 21:31:39 +0530

    actvity2

---
+++ actvity2.py
@@ -0,0 +1,28 @@
+#write a program to create a class  with student and perform the following tasks .
+# Decalare a variable grade 
+# print a sentence inside the class
+# create an orject of class student and see the output
+
+class student:
+    grade=8
+    name="penguin"
+
+    def intro(self):
+        print("hi its me mahi ")
+
+    def show(self):
+            print("my name is ",self.name,"and  i study in grade ",self.grade)
+
+st=student()
+st.intro()
+st.show()
+       
+    
+                
+                
+            
+
+
+    
+
+st=student()
