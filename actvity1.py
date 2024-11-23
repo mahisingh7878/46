8baa451b6a47aafcaa0b647d89b17e966d7047bf
mahisingh4415 <mahisinghadityasingh1232@gmail.com>
2024-11-23 21:14:41 +0530

    actvity1

---
+++ actvity1.py
@@ -0,0 +1,10 @@
+#write a program to create a class  with student and perform the following tasks .
+# Decalare a variable grade 
+# print a sentence inside the class
+# create an orject of class student and see the output
+
+class student:
+    grade=8
+    print("i am in grade ",grade)
+
+st=student()
